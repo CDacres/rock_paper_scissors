e808839b8fc6cde6d390409ae533930e38019446
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-09-11 17:41:46 +0100

    added computer result output for checking, added error for wrong selection

--- main.js
+++ main.js
@@ -12,27 +12,35 @@ if (option === 1) {
 		var player2 = 'p';
 	}
 	if (player1 === player2) {
-		
+		pcOut();
 		alert('It\'s a tie!');
 	} else if (player1 === 'r') {
 		if (player2 === 'p') {
+			pcOut();
 			p2Win();
 		} else {
+			pcOut();
 			p1Win();
 		}
 	} else if (player1 === 'p') {
 		if (player2 === 'r') {
+			pcOut();
 			p1Win();
 		} else {
+			pcOut();
 			p2Win();
+		}
+	} else if (player1 === 's') {
+		if (player2 === 'r') {
+			pcOut();
+			p2Win();
+		} else {
+			pcOut();
+			p1Win();
 		}
 	} else {
-		if (player2 === 'r') {
-			p2Win();
-		} else {
-			p1Win();
-		}
-	} 
+		cheat();
+	}
 
 } else if (option === 2) {
 	alert('You have selected Two Player!');
@@ -43,39 +51,51 @@ if (option === 1) {
 	} else if (player1 === 'r') {
 		if (player2 === 'p') {
 			p2Win();
-		} else {
+		} else if (player2 === 's') {
 			p1Win();
+		} else {
+			cheat();
 		}
 	} else if (player1 === 'p') {
 		if (player2 === 'r') {
 			p1Win();
-		} else {
+		} else if (player2 === 's') {
 			p2Win();
+		} else {
+			cheat();
 		}
-	} else {
+	} else if (player1 === 's') {
 		if (player2 === 'r') {
 			p2Win();
-		} else {
+		} else if (player2 === 'p') {
 			p1Win();
+		} else {
+			cheat();
 		}
-	} 
+	} else {
+		cheat();
+	}
 } else {
 	alert('Error. \n Please select a valid game option.')
 }
 
-function choiceSelect (num) {
+function choiceSelect(num) {
 	var player = prompt('Player ' + num +', please select your choice: \nRock -  Enter r\nPaper - Enter p\nScissors - Enter s').toLowerCase();
 	return player;
 }
 
-function p1Win () {
+function p1Win() {
 	alert('Player 1 wins!');
 }
 
-function p2Win () {
+function p2Win() {
 	alert('Player 2 wins!');
 }
 
-function pcOut () {
+function pcOut() {
 	alert('Player 2 (the computer) selected ' + player2 + '.');
+}
+
+function cheat() {
+	alert('Error! \nPlayers need to select a valid option.');
 }
